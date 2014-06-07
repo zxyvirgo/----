8a243d1a61a6f507ee6fa224012f6b83f9f3cfc7
zxyvirgo <zxyvirgo@aliyun.com>
2014-06-07 19:35:03 +0800

    change comment

--- workspace/one/src/source/Three.java
+++ workspace/one/src/source/Three.java
@@ -9,9 +9,7 @@ public class Three {
 	 */
 
 	 
-	/*通过递归的方式，读取每一层，如果要提高效率的话，应该在TNode里面添加属性level，
-	 * 但是本题中的TNode里面已经给出了结构，所以只能这样处理，不然记录层不方便。
-	 * 
+	/*通过递归的方式，只会读取到指定的层。
 	 */
 	 
 	public void TreeLevel(TNode tree, int n) {
